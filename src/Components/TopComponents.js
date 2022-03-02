7fb8ebb02732a02792deadfbfd4c6999071adce9
Mathias Andersen <mrua@itu.dk>
2022-03-02 14:39:20 +0100

    Added: TopComponents.js file

---
+++ src/Components/TopComponents.js
@@ -0,0 +1,15 @@
+import Graph from "../Components/Graph";
+
+function TopComponents(){
+    return (
+        <div>
+            <Graph/>
+        </div>
+    )
+}
+
+export default TopComponents
+
+
+
+
