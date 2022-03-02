f59329dceea30d534dd28df52c06ca680d3e1b3b
Jepmoltho <jept@itu.dk>
2022-03-02 17:30:01 +0100

    All possible categories and asset card created using conditional rendering

--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -51,10 +51,26 @@ function Dashboard() {
         <h2>Welcome {currentUser.get("username")}</h2>
         <br />
         <Category title="Stocks" />
-        <Asset />
         <br />
-        <AddCategory />
+        <AddCategory type="automatic" />
+        <br />
+        <AddCategory type="manual" />
+        <br />
+        <Asset normal />
+        <br />
+        <Asset debt />
+        <br />
+        <Asset realestate />
+        <br />
         <EditAsset />
+        <br />
+        <EditAsset realestateman />
+        <br />
+        <EditAsset realestateauto />
+        <br />
+        <EditAsset bankman />
+        <br />
+        <EditAsset bankauto />
         <br />
         <button onClick={doUserLogOut}>Logout</button>
       </Container>
