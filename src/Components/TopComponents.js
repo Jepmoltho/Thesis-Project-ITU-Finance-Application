c97b646739370476989b26dea71ad54e1d1657f1
Mathias Andersen <mrua@itu.dk>
2022-03-03 15:56:08 +0100

    Styled TopComponent.js

--- src/Components/TopComponents.js
+++ src/Components/TopComponents.js
@@ -9,12 +9,12 @@ import Goal from "./Goal";
 
 function TopComponents(){
     return (
-        <Row>
-            <Col style={{marginRight: "0px"}}>
+        <Row>   
+            <Col className="col-sm-7" style={{ marginRight: "0px", paddingRight: "10px", paddingLeft: "0px"}}>
                 <Graph/>
             </Col>
-            <Col>
-                <Row style={{ marginBottom: "20px" }} className="card">
+            <Col className="col-sm-5" style={{ marginRight: "0px", paddingRight: "0px", paddingLeft: "10px"}}>
+                <Row style={{ marginBottom: "14px", marginLeft: "0px"}} className="card">
                     <Col>
                           <span> 
                             <a style={{ float: "right", fontStyle: "italic"}} href="https://investorjunkie.com/create-asset-allocation/">
@@ -24,13 +24,13 @@ function TopComponents(){
                         <span style={{ float: "right"}}> logo </span>                      
                     </Col>
 
-                    <Col style={{padding: "0px"}}>
-                        <span>
-                            <SectorDiagram/>
+                    <Col style={{paddingLeft: "0px"}}>
+                        <span style={{paddingLeft: "40px"}}>
+                            <SectorDiagram/>    
                         </span>
                     </Col>
                 </Row>
-                <Row>
+                <Row style={{marginTop: "14px", marginLeft: "0px"}}>
                     <Col className="card" style={{ paddingRight: "0px", paddingLeft: "0px", marginRight: "10px"}}>
                         <Overview/>
                     </Col>
