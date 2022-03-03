61bfb3d0043c8771b68398f4b7f886ab841d85d7
Mathias Andersen <mrua@itu.dk>
2022-03-03 13:04:25 +0100

    Added new component: Goal.js

---
+++ src/Components/Goal.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Tag from "./Tag";
+
+
+function Goal(){
+    return (
+        <div  >
+            <Tag text="Goal"/>        
+        </div>
+    
+    )
+}
+
+export default Goal
