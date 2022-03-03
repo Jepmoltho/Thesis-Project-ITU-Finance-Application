e96db6af8e2519be2de9a4c189f75b465c9063b3
Mathias Andersen <mrua@itu.dk>
2022-03-03 14:28:14 +0100

    Styled gaol components

--- src/Components/Goal.js
+++ src/Components/Goal.js
@@ -4,11 +4,25 @@ import Col from "react-bootstrap/Col";
 import Tag from "./Tag";
 
 
-function Goal(){
+function Goal(props){
     return (
-        <div  >
-            <Tag text="Goal"/>        
+        <div style={{ paddingTop:"10px", paddingLeft:"16px", paddingRight:"0px"}}>
+            <div style={{float: "left"}}>
+                <Tag text="Overview"/>
+            </div>
+            <div style={{float: "left", fontSize: "10px"}}>
+                Logo
+            </div>
+            <div style={{float: "right", fontSize: "10px", paddingRight: "10px"}}>
+                edit
+            </div>             
+            <div>
+                <p style={{ color: "#00145E", marginTop: "34px", marginBottom: "7px" }}>Goal</p>     
+            </div>
+            <div> Progress bar</div>
         </div>
+            
+
     
     )
 }
