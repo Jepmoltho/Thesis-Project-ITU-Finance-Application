0d14a43d2b6a4792cd265c3fdc35a1fc6a36132c
Mathias Andersen <mrua@itu.dk>
2022-03-04 14:03:39 +0100

    Ajusted the positioning of all top components

--- src/Components/SectorDiagram.js
+++ src/Components/SectorDiagram.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from "react";
+import React from "react";
 import 'chart.js/auto';
 import { Chart } from 'react-chartjs-2';
 import { Chart as ChartJS, LineController, LineElement, PointElement, LinearScale, Title } from 'chart.js';
