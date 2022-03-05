528b9b04555f8432a040c8ac8d222d87290c7e6a
ednykvalsund <52636934+ednykvalsund@users.noreply.github.com>
2022-03-05 11:22:23 +0100

    Feature: Added 27 CategoryIcon svg files

--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -56,7 +56,6 @@ function Dashboard() {
     return (
       <div>
         <NavigationBar />
-        <ProgressBar completed='60'/>
         <TopComponents/>
         <Container>
           <h2>Welcome {currentUser.get("username")}</h2>
