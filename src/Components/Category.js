7ac086effeeac8b156f362c81895292dbc1d078c
ednykvalsund <52636934+ednykvalsund@users.noreply.github.com>
2022-03-05 12:08:23 +0100

    Refactor: CategoryIcon.js takes props, but the conditional rendering is not done.

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -15,7 +15,7 @@ function Category(props) {
   return (
     <Row className="category">
       <Col className="col-sm-1" style={{ margin: "auto" }}>
-        <CategoryIcon />
+        <CategoryIcon categoryIcon='stocks'/>
       </Col>
       <Col style={{ margin: "auto" }}>
         <p
--- src/Components/CategoryIcon.js
+++ src/Components/CategoryIcon.js
@@ -1,13 +1,27 @@
-function CategoryIcon() {
-  return (
-    <div className="categoryIcon" style={{ textAlign: "center" }}>
-      <img
-        src="/CategoryIcon.png"
-        alt="tempAlt"
-        style={{ maxWidth: "40px", display: "inline" }}
-      ></img>
-    </div>
-  );
+function CategoryIcon(props) {
+  const categoryIcon = props;
+
+  if (props.categoryIcon === 'stocks'){
+    return (
+      <div className="categoryIcon" style={{ textAlign: "center" }}>
+        <img
+          src="/StocksCategoryIcon.svg"
+          alt="tempAlt"
+          style={{ maxWidth: "40px", display: "inline" }}
+        ></img>
+      </div>
+    );
+    } else {
+      return (
+        <div className="categoryIcon" style={{ textAlign: "center" }}>
+          <img
+            src="/WindpowerCategoryIcon.svg"
+            alt="tempAlt"
+            style={{ maxWidth: "40px", display: "inline" }}
+          ></img>
+        </div>
+      );
+    }
 }
 
 export default CategoryIcon;
