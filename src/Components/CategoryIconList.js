35a61c795738ba79e5be14d19b8bd66676fc5f90
Mathias Andersen <mrua@itu.dk>
2022-03-07 15:18:01 +0100

    Created the icon box, scrollbar and css postioning

---
+++ src/Components/CategoryIconList.js
@@ -0,0 +1,34 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+
+function CategoryIconList(){
+
+      
+    return (
+
+        
+        <Row className="col-sm-6 justify-content-md-center" 
+            style={{  overflowY: "scroll", maxHeight: "270px", backgroundColor: "#cdced2", borderRadius: "5.87", marginTop: "auto", marginBottom: "auto"}}>
+                
+                <Col className="col-sm-6" style={{padding: "0px", margin: "0px"}}>
+                List 1
+                dasdsad
+                sadasd
+                dasdsadasd
+                as
+                dasdsadd
+                asyncdasdasd
+                sadasd
+
+                </Col>
+                
+                <Col className="col-sm-6" style={{padding: "0px", margin: "0px"}}>
+                List 2
+                </Col>
+        </Row>
+    )            
+}
+
+export default CategoryIconList
