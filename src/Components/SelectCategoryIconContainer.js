8ba61c4368f1638884b26bc2ab0a8b31f7fd5fe0
Mathias Andersen <mrua@itu.dk>
2022-03-08 10:16:40 +0100

    Added Button styling

--- src/Components/SelectCategoryIconContainer.js
+++ src/Components/SelectCategoryIconContainer.js
@@ -1,15 +1,19 @@
+import { hover } from "@testing-library/user-event/dist/hover";
 import "bootstrap/dist/css/bootstrap.css";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import CategoryIcon from "./CategoryIcon";
 
 function SelectCategoryIconContainer (props){
+    
     return (
-        <Row name="icon container" className="col-sm-6">
-            <Col name="img and text positioning" style={{ paddingTop: "10px", paddingBottom: "10px", paddingRight: "5px", paddingLeft: "0px"}}>
-                <CategoryIcon categoryIcon={props.icon} />
-                <span > {props.icon} </span>            
-            </Col>  
+        <Row name="icon container" className="col-sm-6" style={{ marginTop: "10px"}}>
+            <button className="button" >
+                <Col name="img and text positioning" style={{textAlign:"left", paddingTop: "10px", paddingBottom: "10px", paddingRight: "0px", paddingLeft: "0px"}}>
+                    <CategoryIcon categoryIcon={props.icon} />
+                    <span > {props.icon} </span>            
+                </Col>  
+            </button>
         </Row>
     )
 }
