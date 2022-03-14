66f288693e31d9eb7dfa51c958c7a2bd2fef4827
Jepmoltho <jept@itu.dk>
2022-03-14 14:28:51 +0100

    Feature: Initial rendering of add category onClick working: Not cleaned up

--- src/Components/AddCategory.js
+++ src/Components/AddCategory.js
@@ -34,51 +34,51 @@ function AddCategory(props) {
 
   if (props.type === "automatic") {
     return (
-      <Row className="addCategory">
-        <Col className="col-sm-1" style={{ margin: "auto" }}></Col>
-        <Col style={{ margin: "auto" }}>
-          <FormControl fullWidth>
-            <InputLabel size="small" id="demo-simple-select-label">
-              Category name
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              label="Category name"
-              onChange={handleChangeInputSelect}
-              value={inputSelect}
-              size="small"
-            >
-              {/*Note: You can use int for value if that makes quering easier*/}
-              <MenuItem value={"Stocks"}>Stocks</MenuItem>
-              <MenuItem value={"Real Estate"}>Real Estate</MenuItem>
-              <MenuItem value={"Crypto"}>Crypto</MenuItem>
-              <MenuItem value={"Bank account"}>Bank account</MenuItem>
-              <MenuItem value={"Create your own category"}>
-                <i>Create your own category</i>
-              </MenuItem>
-            </Select>
-          </FormControl>
-        </Col>
-        <Col style={{ margin: "auto" }}></Col>
-        <Col style={{ margin: "auto" }}></Col>
-        <Col
-          className="col-sm-1"
-          style={{ margin: "auto", paddingLeft: "0px" }}
-        >
-          <ButtonGroup primaryText="Save" secondaryText="Cancel" />
-        </Col>
-        <Col className="col-sm-1">
-          <Icon delete />
-        </Col>
-      </Row>
+      <div className="addCategory">
+        <Row className="addCategory">
+          <Col className="col-sm-1" style={{ margin: "auto" }}></Col>
+          <Col style={{ margin: "auto" }}>
+            <FormControl fullWidth>
+              <InputLabel size="small" id="demo-simple-select-label">
+                Category name
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Category name"
+                onChange={handleChangeInputSelect}
+                value={inputSelect}
+                size="small"
+              >
+                {/*Note: You can use int for value if that makes quering easier*/}
+                <MenuItem value={"Stocks"}>Stocks</MenuItem>
+                <MenuItem value={"Real Estate"}>Real Estate</MenuItem>
+                <MenuItem value={"Crypto"}>Crypto</MenuItem>
+                <MenuItem value={"Bank account"}>Bank account</MenuItem>
+                <MenuItem value={"Create your own category"}>
+                  <i>Create your own category</i>
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Col>
+          <Col style={{ margin: "auto" }}></Col>
+          <Col style={{ margin: "auto" }}></Col>
+          <Col
+            className="col-sm-1"
+            style={{ margin: "auto", paddingLeft: "0px" }}
+          >
+            <ButtonGroup primaryText="Save" secondaryText="Cancel" />
+          </Col>
+          <Col className="col-sm-1">
+            <Icon delete />
+          </Col>
+        </Row>
+      </div>
     );
-  }
-
-  if (props.type === "manual") {
+  } else {
     return (
-      <div>
-        <Row className="addCategory" style={{ zIndex: "-1" }}>
+      <div className="addCategory">
+        <Row className="" style={{ zIndex: "-1" }}>
           <Col className="col-sm-1" style={{ margin: "auto" }}></Col>
           <Col style={{ margin: "auto" }}>
             <TextField
--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -1,28 +1,61 @@
 import Parse from "parse";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Category from "../Components/Category";
+//import Category from "../Components/Category";
 import Container from "react-bootstrap/Container";
-import Asset from "../Components/Asset";
+//import Asset from "../Components/Asset";
 import AddCategory from "../Components/AddCategory";
-import EditAsset from "../Components/EditAsset";
+//import EditAsset from "../Components/EditAsset";
 import NavigationBar from "../Components/NavigationBar";
 import TopComponents from "../Components/TopComponents";
+import AddCategoryBtn from "../Components/AddCategoryBtn";
 
 function Dashboard() {
   const navigate = useNavigate();
+
+  //const [categoryList, setCategoryList] = useState([]);
+  // const [state, setState] = useState("off");
+  // function handleChangeState() {
+  //   setState("on");
+  // }
+  // function addCategory() {
+  //   if (state === "on") {
+  //     return <AddCategory />;
+  //   } else return <AddCategory />;
+  // }
+
+  //const [components, setComponents] = useState(["Sample Component"]);
+  const [components, setComponents] = useState(["Sample Component"]);
+
+  const MyButton = (props) => {
+    return (
+      <button className="AddButton" onClick={props.onClick}>
+        activate
+      </button>
+    );
+  };
+
+  function addComponent() {
+    //setComponents([...components, "Sample Component"])
+    setComponents([...components, "Sample Component"]);
+  }
+
+  //User login/logout related
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
+    //User login/logout related
     getCurrentUser();
   }, []);
 
+  //User login/logout related
   async function getCurrentUser() {
     const currentUser = await Parse.User.current();
     setCurrentUser(currentUser);
     return currentUser;
   }
 
+  //User login/logout related
   const doUserLogOut = async function () {
     try {
       await Parse.User.logOut();
@@ -55,7 +88,27 @@ function Dashboard() {
           <h2>Welcome {currentUser.get("username")}</h2>
           <TopComponents />
           <br />
-          <Category title="Stocks" />
+          {components.map((item, i) => (
+            <AddCategory />
+          ))}
+          <br />
+          <button onClick={addComponent}>Add component</button>
+          <AddCategoryBtn />
+          <br />
+          <button onClick={doUserLogOut}>Logout</button>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
+
+/*
+{state === "off" && <AddCategory />}
+
+
+<Category title="Stocks" />
           <br />
           <AddCategory type="automatic" />
           <br />
@@ -76,12 +129,4 @@ function Dashboard() {
           <EditAsset bankman />
           <br />
           <EditAsset bankauto />
-          <br />
-          <button onClick={doUserLogOut}>Logout</button>
-        </Container>
-      </div>
-    );
-  }
-}
-
-export default Dashboard;
+*/
