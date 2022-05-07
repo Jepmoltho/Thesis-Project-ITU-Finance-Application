fe46b3b7f4a2c77d629e6ddc50cf2e42c036a5ba
ednykvalsund <52636934+ednykvalsund@users.noreply.github.com>
2022-05-07 17:43:24 +0200

    Revert "Revert "Delete profile""

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -123,6 +123,7 @@ function NavigationBar(props) {
             >
               <MenuItem onClick={handleCloseUserMenu}>Profile</MenuItem>
               <MenuItem onClick={props.logout}>Logout</MenuItem>
+              <MenuItem onClick={props.deleteProfile}>Delete profile</MenuItem>
             </Menu>
           </Box>
         </Toolbar>
